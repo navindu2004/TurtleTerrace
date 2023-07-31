af83adfc92b265b2cd43876939c0595166f5fb33
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2023-08-01 00:50:23 +0530

    table insertion

--- ticketsjavas.js
+++ ticketsjavas.js
@@ -76,3 +76,42 @@ function toggleDay(day) {
       localStorage.setItem("selectedDay", selectedTimestamp);
     }
   }
+
+
+
+//function to handle the table contents
+var selectedRow = null;
+function onFormSubmit(){
+    // var formData = readFormData();
+    // if(selectedRow == null)
+    //     insertNewRecord(formData);
+    // else
+    //     updateRecord(formData);
+    // resetForm();
+}
+
+//retrieve the data from the form
+function readFormData(){
+    var formData = {};
+    formData["productCode"] = document.getElementById("productCode").value;
+    formData["product"] = document.getElementById("product").value;
+    formData["qty"] = document.getElementById("qty").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
+
+//insert the data
+function insertNewRecord(data){
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    var cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.productCode;
+    var cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.product;
+    var cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.qty;
+    var cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.perPrice;
+    var cell5 = newRow.insertCell(4);
+    cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`;
+}
