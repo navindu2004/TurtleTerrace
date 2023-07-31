eb681df1b07084edd2d0826c78042210784fbba3
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2023-08-01 01:48:10 +0530

    final commit for today

--- ticketsjavas.js
+++ ticketsjavas.js
@@ -82,12 +82,15 @@ function toggleDay(day) {
 //function to handle the table contents
 var selectedRow = null;
 function onFormSubmit(){
-    // var formData = readFormData();
-    // if(selectedRow == null)
-    //     insertNewRecord(formData);
-    // else
-    //     updateRecord(formData);
-    // resetForm();
+    event.preventDefault();
+    var formData = readFormData();
+    if(selectedRow == null){
+        insertNewRecord(formData);
+    }
+    else{
+        updateRecord(formData);
+}
+    resetForm();
 }
 
 //retrieve the data from the form
@@ -113,5 +116,39 @@ function insertNewRecord(data){
     var cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.perPrice;
     var cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<button>Edit</button> <button>Delete</button>`;
+    cell5.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`;
 }
+
+//edit the data
+function onEdit(td){
+    selectedRow = td.parentElement;
+    document.getElementById('productCode').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('product').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('qty').value = selectedRow.cells[2].innerHTML;
+    document.getElementById('perPrice').value = selectedRow.cells[3].innerHTML;
+}
+
+function updateRecord(formData){
+selectedRow.cells[0].innerHTML = formData.productCode;
+selectedRow.cells[1].innerHTML = formData.product;
+selectedRow.cells[2].innerHTML = formData.qty;
+selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+//delete the data
+function onDelete(td){
+    if(confirm('Are you sure to delete this record?')){
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+    }
+    resetForm();
+}
+
+//reset the data
+function resetForm(){
+    document.getElementById('productCode').value = '';
+    document.getElementById('product').value = '';
+    document.getElementById('qty').value = '';
+    document.getElementById('perPrice').value = '';
+}
+
