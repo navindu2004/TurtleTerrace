a183769e491ff429e2edfc0e22d47940e25a84b3
Navindu Nimnal Premaratne <130588741+navindu2004@users.noreply.github.com>
2023-08-02 16:35:04 +0530

    date, tickets and total payable working

--- ticketsjavas.js
+++ ticketsjavas.js
@@ -1,98 +1,87 @@
-const daysTag = document.querySelector(".days"),
-currentDate = document.querySelector(".current-date"),
-prevNextIcon = document.querySelectorAll(".icons span");
+//FOR THE CALENDAR
+// const daysTag = document.querySelector(".days"),
+// currentDate = document.querySelector(".current-date"),
+// prevNextIcon = document.querySelectorAll(".icons span");
 
 
-let date = new Date(),
-currYear = date.getFullYear(),
-currMonth = date.getMonth();
+// let date = new Date(),
+// currYear = date.getFullYear(),
+// currMonth = date.getMonth();
 
 
-// storing full name of all months in array
-const months = ["January", "February", "March", "April", "May", "June", "July",
-              "August", "September", "October", "November", "December"];
+// // storing full name of all months in array
+// const months = ["January", "February", "March", "April", "May", "June", "July",
+//               "August", "September", "October", "November", "December"];
 
 
-const renderCalendar = () => {
-    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+// const renderCalendar = () => {
+//     let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
 
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
-    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+//     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+//     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
 
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
-    let liTag = "";
-
-
-    for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
-        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
-    }
-    for (let i = 1; i <= lastDateofMonth; i++) {
-
-        liTag += `<li class="day" onclick="to(${i})">${i}</li>`;
-    }
-    for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
-        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
-    }
+//     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+//     let liTag = "";
 
 
-    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
-    daysTag.innerHTML = liTag;
-}
-renderCalendar();
+//     for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+//         liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+//     }
+//     for (let i = 1; i <= lastDateofMonth; i++) {
+
+//         liTag += `<li class="day" onclick="to(${i})">${i}</li>`;
+//     }
+//     for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+//         liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+//     }
 
 
-prevNextIcon.forEach(icon => { // getting prev and next icons
-    icon.addEventListener("click", () => { // adding click event on both icons
-        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
-        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+//     currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+//     daysTag.innerHTML = liTag;
+// }
+// renderCalendar();
 
-        if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
-            // creating a new date of current year & month and pass it as date value
-            date = new Date(currYear, currMonth, new Date().getDate());
-            currYear = date.getFullYear(); // updating current year with new date year
-            currMonth = date.getMonth(); // updating current month with new date month
-        } else {
-            date = new Date(); // pass the current date as date value
-        }
-        renderCalendar(); // calling renderCalendar function
-    });
-});
 
-// Function to handle day selection
-function toggleDay(day) {
-    const selectedDayElement = document.querySelector(
-      `.days li:nth-child(${day + firstDayofMonth})`
-    );
+// prevNextIcon.forEach(icon => { // getting prev and next icons
+//     icon.addEventListener("click", () => { // adding click event on both icons
+//         // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+//         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+//         if(currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+//             // creating a new date of current year & month and pass it as date value
+//             date = new Date(currYear, currMonth, new Date().getDate());
+//             currYear = date.getFullYear(); // updating current year with new date year
+//             currMonth = date.getMonth(); // updating current month with new date month
+//         } else {
+//             date = new Date(); // pass the current date as date value
+//         }
+//         renderCalendar(); // calling renderCalendar function
+//     });
+// });
+
+// // Function to handle day selection
+// function toggleDay(day) {
+//     const selectedDayElement = document.querySelector(
+//       `.days li:nth-child(${day + firstDayofMonth})`
+//     );
   
-    if (selectedDayElement.classList.contains("active")) {
-      // Day is already selected, so deselect it
-      selectedDayElement.classList.remove("active");
-      localStorage.removeItem("selectedDay");
-    } else {
-      // Day is not selected, so select it
-      selectedDayElement.classList.add("active");
-      const selectedDay = new Date(currYear, currMonth, day);
-      const selectedTimestamp = selectedDay.getTime();
-      localStorage.setItem("selectedDay", selectedTimestamp);
-    }
-  }
+//     if (selectedDayElement.classList.contains("active")) {
+//       // Day is already selected, so deselect it
+//       selectedDayElement.classList.remove("active");
+//       localStorage.removeItem("selectedDay");
+//     } else {
+//       // Day is not selected, so select it
+//       selectedDayElement.classList.add("active");
+//       const selectedDay = new Date(currYear, currMonth, day);
+//       const selectedTimestamp = selectedDay.getTime();
+//       localStorage.setItem("selectedDay", selectedTimestamp);
+//     }
+//   }
+
+//END OF CALENDAR PART
+  
 
 
-
-
-//function to handle the table contents
-var selectedRow = null;
-function onFormSubmit(){
-    event.preventDefault();
-    var formData = readFormData();
-    if(selectedRow == null){
-        insertNewRecord(formData);
-    }
-    else{
-        updateRecord(formData);
-}
-    resetForm();
-}
 
 //retrieve the data from the form
 
@@ -254,3 +243,55 @@ function resetForm(){
     document.getElementById('cf').value = '';
 }
 
+
+
+
+
+
+
+
+function updateLocalStorage() {
+    // Get all the checkboxes with name 'timeslot'
+    const checkboxes = document.getElementsByName('timeslot');
+
+    // Create an array to store selected time slots
+    const selectedTimeSlots = [];
+
+    // Loop through all checkboxes to find selected ones
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            selectedTimeSlots.push(checkbox.value);
+        }
+    });
+
+    // Store the selected time slots in local storage
+    localStorage.setItem('selectedtimeslot', JSON.stringify(selectedTimeSlots));
+}
+
+
+//function to handle the table contents
+var selectedRow = null;
+
+function onFormSubmit(){
+    event.preventDefault();
+    var formData = readFormData();
+    if(selectedRow == null){
+        insertNewRecord(formData);
+    }
+    else{
+        updateRecord(formData);
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const selectedTimeSlots = JSON.parse(localStorage.getItem('selectedtimeslot'));
+    if (selectedTimeSlots && selectedTimeSlots.length > 0) {
+        const checkboxes = document.getElementsByName('timeslot');
+        checkboxes.forEach(checkbox => {
+            if (selectedTimeSlots.includes(checkbox.value)) {
+                checkbox.checked = true;
+            }
+        });
+    }
+});
+}
+
+document.addEventListener('DOMContentLoaded', onFormSubmit);
